536b9ac2c6f530e1709d93a60a5cc87509510594
stxkxs <brandon.stokes@hey.com>
2025-11-28 23:09:55 -0800

    fix: simplified context object
    - change bin/bootstrap.ts to read from 'self' context instead of 'handshake'
    - update cdk.context.json example to use 'subscriber' key
    - remove unused cdk.version field from example context

--- bin/bootstrap.ts
+++ bin/bootstrap.ts
@@ -5,7 +5,7 @@ import * as cdk from "aws-cdk-lib"
 import { BootstrapStack } from "../lib/bootstrap-stack"
 
 const app = new cdk.App()
-const name = app.node.getContext("handshake")?.name
+const name = app.node.getContext("self")?.name
 
 new BootstrapStack(app, "fastish", name, {
   env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
